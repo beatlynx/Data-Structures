bbc20ce6cbb2f7a7e93a029b001f5ca5f969a530
Samuel Elmer <selmer@tamu.edu>
2021-09-26 19:47:34 -0500

    Fix array implementation & overloading [] operator

--- array.h
+++ array.h
@@ -2,38 +2,88 @@
 
 #include <iostream>
 
+using std::cout, std::endl;
 template <typename T>
 class Array {
-    Array<T>() {
-        arr = nullptr;
-        capacity = 0;
-        current_size = 0;
-    }
-    Array<T>(int n) {
-        arr = new T(n);
-        capacity = n;
-        current_size = 0;
-    }
-    ~Array<T>() {
-        delete[] arr;
-    }
-    Array<T>(const Array<T>& b) {
-        capacity = b.capacity;
-        current_size = b.size;
-        arr = new T(capacity);
-        for(size_t i=0; i<current_size; i++) {
-            arr[i] = b.arr[i];
+    public:
+        Array() : arr(nullptr), capacity(0), current_size(0) {}
+        Array(int n) : arr(new T[n]), capacity(n), current_size(0) {}
+        ~Array() {
+            if(arr!=nullptr) delete[] arr;
         }
-    }
+        Array(const Array<T>& b) {
+            capacity = b.capacity;
+            current_size = b.size;
+            arr = new T(capacity);
+            for(size_t i=0; i<current_size; i++) {
+                arr[i] = b.arr[i];
+            }
+        }
+        Array* operator=(const Array<int>& b) {
+            return this;
+        }
+        T& operator[](int index) {
+            if(index>=(int)current_size || index<0) {
+                throw std::invalid_argument("Out of bounds.");
+            }
+            return arr[index];
+        }
+        int size() {
+            return (int)current_size;
+        };
+        bool contains(T toFind) {
+            if(find(toFind) == -1) return false;
+            return true;
+        };
+        int find(T toFind) {
+            for(size_t i=0; i<current_size; i++) {
+                if(arr[i] == toFind) {
+                    return i;
+                }
+            }
+            return -1;
+        };
+        void insert(T toInsert) {
+            if(current_size+1 >= capacity) {
+                resize();
+            }
+            arr[current_size] = toInsert;
+            current_size++;
+        };
+        bool remove(T toRemove) {
+            int index = find(toRemove);
+            if(index<0) return false;
+                
+            for(;index<(int)current_size; index++) {
+                arr[index] = arr[index+1];
+            }
+            current_size--;
+            return true;
+        };
+        bool empty() {
+            if(current_size<1) return true;
+            return false;
+        }
+        void print() {
+            cout << "[";
+            for(size_t i=0; i<current_size-1; i++) {
+                cout << arr[i] << ", ";
+            }
+            cout << arr[current_size-1] << "]" << endl;
+        }
     private:
         T* arr;
         size_t capacity;
         size_t current_size;
-        void resize();
-    public:
-        int size();
-        bool contains(T toFind);
-        int find(T toFind);
-        void insert(T toInsert);
-        bool remove(T toRemove);
+        void resize() {
+            if(capacity==0) {
+            capacity = 2;
+            } else capacity *= 2;
+            T* newarr = new T[capacity];
+            for(size_t i=0; i<current_size; i++) {
+                newarr[i] = arr[i];
+            }
+            if(arr!=nullptr) delete[] arr;
+            arr = newarr;
+        };
 };
--- tests.cpp
+++ tests.cpp
@@ -1,11 +1,39 @@
 #include "array.h"
 #include <iostream>
+#include <vector>
 
 using std::cout, std::endl;
 
 int main() {
 
     cout << "Test" << endl;
+
+    Array<int> a;
+
+    cout << a.size() << endl;
+
+    cout << a.empty() << endl;
+
+    a.insert(1);
+    a.insert(2);
+    a.insert(3);
+    a.insert(4);
+
+    cout << a.size() << endl;
+
+    a.print();
+
+    a.remove(3);
+
+    cout << a.size() << endl;
+
+    a.print();
+
+    cout << a[0] << endl;
+
+    a[0] = 100;
+
+    a.print();
 
     return 0;
 }
